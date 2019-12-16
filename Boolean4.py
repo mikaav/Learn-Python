8bb01b345b8d73c4ed4a4b677ec7bcb118a9e604
Karina Kantuni <avagyan_mika@mail.ru>
2019-12-16 23:41:27 +0400

    Add Boolean3 and Boolean4

---
+++ Boolean4.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+if a >2:
+    print("Chislo A bolshe 2")
+else:
+    print("Chilso A Menshe 2")
+if b <= 3:
+    print("Chislo B menshe ili ravno 3")
+else:
+    print("Chislo B bolshe 3")
